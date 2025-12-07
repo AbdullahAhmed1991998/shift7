08d2f3e6585151fe36ca4bbf9054da87bce41f48
abdullah <abdullahahmed1991998@gmail.com>
2025-12-07 19:43:27 +0300

    Deploy

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -5,7 +5,7 @@ import GoogleMaps
 import UserNotifications
 
 @main
-class AppDelegate: FlutterAppDelegate, UNUserNotificationCenterDelegate {
+class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
